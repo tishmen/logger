bccdb0bf214cafa61084a4d60b46429c0327c756
tishmen <topuzov.work@gmail.com>
2016-03-29 04:13:19 +0200

    admin for test model

--- src/logger/admin.py
+++ src/logger/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TestModel
+
+
+class ReadOnlyAdmin(admin.ModelAdmin):
+
+    list_display_links = None
+
+    def has_add_permission(self, request, obj=None):
+        pass
+
+    def has_delete_permission(self, request, obj=None):
+        pass
+
+    def get_actions(self, request):
+        pass
+
+
+@admin.register(TestModel)
+class TestModelAdmin(ReadOnlyAdmin):
+
+    pass
